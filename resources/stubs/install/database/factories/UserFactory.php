4781c19a7985c184a2210a6e602ddfd30ae404f8
bastinald <bastinald@icloud.com>
2021-05-20 16:56:04 -0400

    user factory

---
+++ resources/stubs/install/database/factories/UserFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return app($this->model)->definition($this->faker);
+    }
+}
